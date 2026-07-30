65cbd471cdb510840ef11740a17fc914dd41e52c
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-30 14:32:01 +0500

    Insert data in table

--- Basics/01-First_PostgresSQL.sql
+++ Basics/01-First_PostgresSQL.sql
@@ -1,9 +1,26 @@
 -- CREATE DATABASE postgressql_basics;
 
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    age INT NOT NULL CHECK(age>=18),
-    created_at TIMESTAMP DEFAULT NOW()
-)
+-- CREATE TABLE students (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     email TEXT NOT NULL UNIQUE,
+--     age INT NOT NULL CHECK(age>=18),
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+
+-- SELECT * FROM students;
+
+-- INSERT INTO students (
+--     name,
+--     email,
+--     age
+-- )
+-- VALUES (
+--     'Paras',
+--     'paras@gmail.com',
+--     '19'
+-- )
+
+-- SELECT * FROM students;
+
+-- DROP TABLE students;
