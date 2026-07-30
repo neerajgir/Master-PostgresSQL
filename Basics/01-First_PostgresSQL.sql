7cb72ffb0bd5679efca231784b072d76fb53f31f
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-30 14:13:11 +0500

    create db and table in database

--- Basics/01-First_PostgresSQL.sql
+++ Basics/01-First_PostgresSQL.sql
@@ -1,1 +1,9 @@
 -- CREATE DATABASE postgressql_basics;
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    age INT NOT NULL CHECK(age>=18),
+    created_at TIMESTAMP DEFAULT NOW()
+)
