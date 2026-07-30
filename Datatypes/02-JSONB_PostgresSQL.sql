42bd0a7e05b787fc4b1287039dd01eb0d2eee7bc
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-30 18:01:19 +0500

    Comment out users table creation and previous SELECT queries; add new user insertion example.

--- Datatypes/02-JSONB_PostgresSQL.sql
+++ Datatypes/02-JSONB_PostgresSQL.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL ,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    -- preferences JSONB NOT NULL,
-    preferences JSONB DEFAULT '{}'::JSONB,
-    created_at TIMESTAMP DEFAULT NOw()
-);
+-- CREATE TABLE users (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     name VARCHAR(255) NOT NULL ,
+--     email VARCHAR(255) NOT NULL UNIQUE,
+--     -- preferences JSONB NOT NULL,
+--     preferences JSONB DEFAULT '{}'::JSONB,
+--     created_at TIMESTAMP DEFAULT NOw()
+-- );
 
 
 -- INSERT INTO users (
@@ -32,10 +32,19 @@ CREATE TABLE users (
 --     }'
 -- );
 
--- SELECT * FROM users;
 
-SELECT name, preferences FROM users;
--- SELECT name, preferences->>'theme' AS theme FROM users;
--- SELECT name AS username, preferences->>'theme' AS theme FROM users;
-SELECT name AS username, preferences->>'theme' AS theme, preferences->>'language' AS user_language FROM users;
+INSERT INTO users (
+    name,
+    email
+)
+VALUES (
+    'Tulsi Gir',
+    'tulsi@gmail.com'
+);
+SELECT * FROM users;
+
+-- SELECT name, preferences FROM users;
+-- -- SELECT name, preferences->>'theme' AS theme FROM users;
+-- -- SELECT name AS username, preferences->>'theme' AS theme FROM users;
+-- SELECT name AS username, preferences->>'theme' AS theme, preferences->>'language' AS user_language FROM users;
 
