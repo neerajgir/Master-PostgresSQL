1598844680cba59a5f03ddd0b3df8dd1d6c1b5d0
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 23:04:11 +0500

    Insert a customer record into the customers table and comment out the table creation SQL statement.

--- Practice/04-JSONBPractice.sql
+++ Practice/04-JSONBPractice.sql
@@ -1,6 +1,22 @@
-CREATE TABLE customers(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    profile JSONB DEFAULT '{}'::JSONB,
-    created_at TIMESTAMP DEFAULT NOw()
+-- CREATE TABLE customers(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     profile JSONB DEFAULT '{}'::JSONB,
+--     created_at TIMESTAMP DEFAULT NOw()
+-- );
+
+
+INSERT INTO customers (
+    name,
+    profile
+)
+VALUES (
+    'Neeraj',
+    '{
+        "city": "Karachi",
+        "phone": "03332203040",
+        "age": 24    
+    }'
 );
+
+SELECT * FROM customers;
