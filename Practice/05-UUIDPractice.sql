5e8b4f809ea10ef7f7e2942ea308dff2014ef6a0
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 23:22:38 +0500

    Comment out the contractors table creation SQL and insert a new contractor record, followed by a SELECT query to retrieve contractors.

--- Practice/05-UUIDPractice.sql
+++ Practice/05-UUIDPractice.sql
@@ -1,6 +1,17 @@
-CREATE Table contractors (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
+-- CREATE Table contractors (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     username VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL UNIQUE,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+
+INSERT INTO contractors (
+    username,
+    email
+)
+VALUES (
+    'Neeraj Goswami',
+    'neerajcontractor@gmail.com'
 );
+
+SELECT * FROM contractors;
