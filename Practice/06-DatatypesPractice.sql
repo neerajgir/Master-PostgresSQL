ce69c8fa1c863a93630353e327bc294100ca5f3c
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-02 00:32:41 +0500

    Comment out the previous orders table creation SQL and insert a new order record, followed by a SELECT query to retrieve orders.

--- Practice/06-DatatypesPractice.sql
+++ Practice/06-DatatypesPractice.sql
@@ -1,8 +1,34 @@
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    total_amount NUMERIC(10, 2),
-    is_paid BOOLEAN DEFAULT true,
-    order_details JSONB DEFAULT '{}'::JSONB,
-    created_at TIMESTAMP DEFAULT NOW()
+-- CREATE TABLE orders (
+--     id SERIAL PRIMARY KEY,
+--     customer_name VARCHAR(255) NOT NULL,
+--     total_amount NUMERIC(10, 2),
+--     is_paid BOOLEAN DEFAULT true,
+--     order_details JSONB DEFAULT '{}'::JSONB,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- )
+
+INSERT INTO orders (
+    customer_name,
+    total_amount,
+    is_paid,
+    order_details
 )
+VALUES (
+    'Neeraj',
+    5000,
+    true,
+    '{
+    "items":[
+        {
+            "product":"Laptop",
+            "qty":2
+        },
+        {
+            "product":"Mouse",
+            "qty":1
+        }
+    ]
+}'
+);
+
+SELECT * FROM orders;
