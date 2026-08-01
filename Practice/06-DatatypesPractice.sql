75c08153e970ad54bea4c166ed5e2f2046226eb4
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-02 00:42:41 +0500

    Comment out the previous order insertion SQL and add new queries to retrieve the first and second products along with their quantities from the orders table.

--- Practice/06-DatatypesPractice.sql
+++ Practice/06-DatatypesPractice.sql
@@ -7,28 +7,37 @@
 --     created_at TIMESTAMP DEFAULT NOW()
 -- )
 
-INSERT INTO orders (
-    customer_name,
-    total_amount,
-    is_paid,
-    order_details
-)
-VALUES (
-    'Neeraj',
-    5000,
-    true,
-    '{
-    "items":[
-        {
-            "product":"Laptop",
-            "qty":2
-        },
-        {
-            "product":"Mouse",
-            "qty":1
-        }
-    ]
-}'
-);
+-- INSERT INTO orders (
+--     customer_name,
+--     total_amount,
+--     is_paid,
+--     order_details
+-- )
+-- VALUES (
+--     'Neeraj',
+--     5000,
+--     true,
+--     '{
+--     "items":[
+--         {
+--             "product":"Laptop",
+--             "qty":2
+--         },
+--         {
+--             "product":"Mouse",
+--             "qty":1
+--         }
+--     ]
+-- }'
+-- );
 
-SELECT * FROM orders;
+-- SELECT * FROM orders;
+-- get product
+SELECT order_details->'items'->0->>'product' AS first_product FROM orders;
+
+-- get quantity
+SELECT (order_details->'items'->0->>'qty')::int AS first_product_qty FROM orders;
+
+-- get second product
+SELECT order_details->'items'->1->>'product' AS second_product
+FROM orders
