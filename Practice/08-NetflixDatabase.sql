3cfee9797c63db10b0e114138242751085556338
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-04 22:41:01 +0500

    Comment out previous Movies table creation SQL and insert a new movie record for 'Edge of Tomorrow', followed by a SELECT query to retrieve movies.

--- Practice/08-NetflixDatabase.sql
+++ Practice/08-NetflixDatabase.sql
@@ -1,8 +1,32 @@
-CREATE TABLE Movies (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    release_year DATE NOT NULL DEFAULT CURRENT_DATE,
-    rating BIGINT DEFAULT 0,
-    is_series BOOLEAN DEFAULT true,
-    details JSONB DEFAULT '{}'::JSONB
+-- CREATE TABLE Movies (
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(100) NOT NULL,
+--     release_year DATE NOT NULL DEFAULT CURRENT_DATE,
+--     rating BIGINT DEFAULT 0,
+--     is_series BOOLEAN DEFAULT true,
+--     details JSONB DEFAULT '{}'::JSONB
+-- );
+
+INSERT INTO Movies (
+    title,
+    release_year,
+    rating,
+    is_series,
+    details
+)
+VALUES(
+    'Edge of Tomorrow',
+    '2014-06-06',
+    7.9,
+    FALSE,
+    '{
+    "genres":["Action","Sci-Fi"],
+     "cast":[
+        "Tom Cruise",
+        "Emily Blunt"
+    ],
+    "duration":"2h 15m"
+    }'
 );
+
+SELECT * FROM Movies;
