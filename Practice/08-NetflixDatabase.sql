c122eecf002983c17cbd470213a641dfff67c73f
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-04 22:50:49 +0500

    Comment out the previous movie insertion SQL for 'Edge of Tomorrow' and add new SELECT queries to retrieve genres, cast, and duration from the Movies table.

--- Practice/08-NetflixDatabase.sql
+++ Practice/08-NetflixDatabase.sql
@@ -7,26 +7,35 @@
 --     details JSONB DEFAULT '{}'::JSONB
 -- );
 
-INSERT INTO Movies (
-    title,
-    release_year,
-    rating,
-    is_series,
-    details
-)
-VALUES(
-    'Edge of Tomorrow',
-    '2014-06-06',
-    7.9,
-    FALSE,
-    '{
-    "genres":["Action","Sci-Fi"],
-     "cast":[
-        "Tom Cruise",
-        "Emily Blunt"
-    ],
-    "duration":"2h 15m"
-    }'
-);
+-- INSERT INTO Movies (
+--     title,
+--     release_year,
+--     rating,
+--     is_series,
+--     details
+-- )
+-- VALUES(
+--     'Edge of Tomorrow',
+--     '2014-06-06',
+--     7.9,
+--     FALSE,
+--     '{
+--     "genres":["Action","Sci-Fi"],
+--      "cast":[
+--         "Tom Cruise",
+--         "Emily Blunt"
+--     ],
+--     "duration":"2h 15m"
+--     }'
+-- );
 
-SELECT * FROM Movies;
+-- SELECT * FROM Movies;
+
+-- genres
+-- SELECT details->'genres'->>0 AS Genres FROM Movies;
+-- SELECT details->'genres'->>1 AS Genres FROM Movies;
+
+-- actor 
+-- SELECT details->'cast'->>0 AS Cast FROM Movies;
+-- duration
+-- SELECT details->>'duration' AS Duration FROM Movies;
