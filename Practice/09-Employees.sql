7af2fe6b5ba315eeae3ce6a6a28b443900804ca1
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-08 00:20:27 +0500

    Enhance Employees table queries: Add a new SELECT query to retrieve employee details with salaries above 80,000, while retaining the existing update and delete functionalities for employee records.

--- Practice/09-Employees.sql
+++ Practice/09-Employees.sql
@@ -27,3 +27,5 @@ UPDATE Employees SET salary = salary * 1.10 WHERE department IN ('IT', 'Engineer
 SELECT * FROM Employees;
 
 DELETE FROM Employees WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, joining_date)) = 0;
+
+SELECT id, full_name, department, salary FROM Employees WHERE salary > 80000.00;
