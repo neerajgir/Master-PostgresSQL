1fa42f6696f63e0c8ab40cb3ea38d80618e885d8
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-10 01:17:16 +0500

    Enhance streaming_users table schema in alterexample.sql: Add email, signup_date, and subscription_type columns to improve user data management and organization, along with a SELECT query to display all user records.

--- Intermediate/02-alterexample.sql
+++ Intermediate/02-alterexample.sql
@@ -2,3 +2,11 @@ CREATE TABLE streaming_users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) 
 );
+
+-- Expand the structure
+ALTER TABLE streaming_users 
+ADD COLUMN email VARCHAR(100) UNIQUE NOT NULL,
+ADD COLUMN signuo_date DATE DEFAULT CURRENT_DATE,
+ADD COLUMN subscription_type VARCHAR(50) DEFAULT 'free';
+
+SELECT * FROM streaming_users;
