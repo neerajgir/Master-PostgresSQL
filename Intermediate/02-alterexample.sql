99a8225a4e4026089cdaabc516cfc88914ada81d
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-10 01:22:22 +0500

    Enhance streaming_users table schema in alterexample.sql: Set NOT NULL constraint on email, add payment_method and last_payment_date columns, rename subscription_type to plan_type, drop payment_method, and modify username column type for improved data integrity and user management.

--- Intermediate/02-alterexample.sql
+++ Intermediate/02-alterexample.sql
@@ -9,4 +9,18 @@ ADD COLUMN email VARCHAR(100) UNIQUE NOT NULL,
 ADD COLUMN signuo_date DATE DEFAULT CURRENT_DATE,
 ADD COLUMN subscription_type VARCHAR(50) DEFAULT 'free';
 
+ALTER TABLE streaming_users
+ALTER COLUMN email SET NOT NULL;
+ALTER TABLE streaming_users
+ADD COLUMN payment_method VARCHAR(50),
+ADD COLUMN last_payment_date DATE;
+ALTER TABLE streaming_users
+RENAME COLUMN subscription_type TO plan_type;
+
+ALTER TABLE streaming_users 
+DROP COLUMN payment_method;
+
+ALTER TABLE streaming_users
+ALTER COLUMN username TYPE VARCHAR(30);
+
 SELECT * FROM streaming_users;
