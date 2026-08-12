55cdd0438f4110ac3303889584d588a4c1cf6bed
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-12 15:03:42 +0500

    start alter practice

---
+++ Intermediate-Practice/02-alter.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE,
+    salary NUMERIC(10, 2),
+    department VARCHAR(50),
+    is_active BOOLEAN DEFAULT true
+);
+
