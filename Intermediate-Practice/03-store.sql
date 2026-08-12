a11122254a6b33e0fea27fd401759561f9737061
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-12 15:45:39 +0500

    Refactor products table structure: Modify column properties for improved data management and enforce constraints for data integrity in the store_products table.

---
+++ Intermediate-Practice/03-store.sql
@@ -0,0 +1,25 @@
+-- Active: 1785411209906@@127.0.0.1@5432@intermediate_practice
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(150),
+    phone VARCHAR(30),
+    city VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT,
+    total NUMERIC(10, 2),
+    status VARCHAR(30),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price NUMERIC(10, 2),
+    stock INT,
+    category VARCHAR(50)
+);
+
