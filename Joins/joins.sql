7344fd72b020b3b0eb2b515e176cd26361eeb41c
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-17 15:22:21 +0500

    Add LEFT JOIN example to joins.sql for enhanced query demonstration

--- Joins/joins.sql
+++ Joins/joins.sql
@@ -29,3 +29,12 @@ SELECT s.name, c.class_name
 FROM students s
 INNER JOIN classes c
 ON s.class_id = c.class_id;
+
+-- 2️⃣ LEFT JOIN (LEFT OUTER JOIN)
+
+SELECT s.name, c.class_name
+FROM students s
+LEFT JOIN classes c
+ON s.class_id = c.class_id;
+
+
