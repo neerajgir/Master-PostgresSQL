ac8ebecbe5618b8ca63e7f58aa20aff1bb743d91
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-17 15:16:17 +0500

    Refactor users table: Set date_of_birth column to NOT NULL, add unique constraint on date_of_birth, and enforce date format validation for improved data integrity and management.

---
+++ Joins/joins.sql
@@ -0,0 +1,31 @@
+-- Active: 1785411209906@@127.0.0.1@5432@joins
+CREATE TABLE classes (
+  class_id INT PRIMARY KEY,
+  class_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE students (
+  student_id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  class_id INT
+);
+
+INSERT INTO classes (class_id, class_name) VALUES
+(101, 'JavaScript'),
+(102, 'Python'),
+(103, 'Java');
+
+INSERT INTO students (student_id, name, class_id) VALUES
+(1, 'Rahul', 101),
+(2, 'Anjali', 102),
+(3, 'Aman', 101),
+(4, 'Neha', NULL);
+
+SELECT * FROM students;
+SELECT * FROM classes;
+
+-- 1️⃣ INNER JOIN
+SELECT s.name, c.class_name
+FROM students s
+INNER JOIN classes c
+ON s.class_id = c.class_id;
